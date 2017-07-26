f2327cde1d152dabce0b1c8373eb3712314554eb
Sameer Rahmani <lxsameer@gnu.org>
2017-07-26 20:29:36 +0100

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hh_example
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hh_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns hh-example.core-test
+  (:require [clojure.test :refer :all]
+            [hh-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
