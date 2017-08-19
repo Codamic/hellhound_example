e1714f7090800dc9b1b86c2a9f61816ff2147e5b
Sameer Rahmani <lxsameer@gnu.org>
2017-08-20 00:11:13 +0100

    Hellhound native routes used as default one

--- src/clj/hh_example/system.clj
+++ src/clj/hh_example/system.clj
@@ -1,11 +1,8 @@
 (ns hh-example.system
   (:require
+   [hellhound.http.route       :as router]
    [hellhound.components.aleph :as aleph]))
 
-(defn handler [req]
-  {:status 200
-   :headers {"content-type" "text/plain"}
-   :body "hello!"})
 
 (def dev-system
-  {:components [(aleph/factory handler)]})
+  {:components [(aleph/factory router/routes)]})
