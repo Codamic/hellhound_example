d75996b7bd7d980b59b97d275fcc1414e9a77459
Sameer Rahmani <lxsameer@gnu.org>
2017-11-02 00:43:59 +0000

    PoC of first weserver example is finished

--- src/clj/components/webserver_example1.clj
+++ src/clj/components/webserver_example1.clj
@@ -1,21 +1,13 @@
 (ns components.webserver-example1
   (:require [hellhound.system :as system]
-            [hellhound.component :As hcomp]
+            [hellhound.component :as hcomp]
             [hellhound.components.webserver :as web]
             [hellhound.http.route :as routes]
-            [bidi.bidi :as bidi]))
-
-;; (routes/defroutes default-routes
-;;   [context]
-;;   ["/" {:get {"/" routes/hello
-;;               "ws/" routes/ws}}])
+            [manifold.stream :as s]))
 
 (defn default-routes
   [context]
-  (println "<<<<<<<<<<")
-  (bidi.ring/make-handler
-   ["/" {:get {"/" routes/hello
-               "ws/" routes/ws}}]))
+  (routes/ws (:input context) (:output context)))
 
 (def system
   {:components [(web/factory default-routes)]})
@@ -23,4 +15,6 @@
 (defn main
   []
   (system/set-system! system)
-  (system/start!))
+  (system/start!)
+  (let [output (hcomp/output (system/get-component :hellhound.components.webserver/webserver))]
+    (s/consume #(println "Received: " %) output)))
