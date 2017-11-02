940fd0aaeb56aa47d2bfd305cef9455c96995b3b
Sameer Rahmani <lxsameer@gnu.org>
2017-11-02 23:49:33 +0000

    First PoC of pedestal route with aleph finished

--- src/clj/components/webserver_example1.clj
+++ src/clj/components/webserver_example1.clj
@@ -1,16 +1,33 @@
 (ns components.webserver-example1
-  (:require [hellhound.system :as system]
-            [hellhound.component :as hcomp]
-            [hellhound.components.webserver :as web]
-            [hellhound.http.route :as routes]
-            [manifold.stream :as s]))
+  (:require
+   [io.pedestal.http.route :as router]
+   [hellhound.system :as system]
+   [hellhound.component :as hcomp]
+   [hellhound.components.webserver :as web]
+   [hellhound.http.route :as routes]
+   [manifold.stream :as s]))
 
-(defn default-routes
+(def default-routes
+  (io.pedestal.http.route.map-tree/router
+   (router/expand-routes
+                  #{{:host "localhost" :scheme :http :port 3000}
+                    ["/" :get `routes/hello :route-name :home]})))
+     ;;["/ws" :get [(routes/ws-interceptor (:input context) (:output context))] :route-name :ws]}))
+(defn default
   [context]
-  (routes/ws (:input context) (:output context)))
+  (fn [req]
+    (let [matched (io.pedestal.http.route.router/find-route default-routes (assoc req :path-info (:uri req)))
+          matched-fn (:interceptors matched)]
+      (if matched
+        (do
+          (println "Returned: " (keys (io.pedestal.interceptor.chain/execute (assoc context :req req) matched-fn)))
+          (:response (io.pedestal.interceptor.chain/execute (assoc context :req req) matched-fn)))
+        {:status 404 :headers {} :body "Note found"}))))
+
+
 
 (def system
-  {:components [(web/factory default-routes)]})
+  {:components [(web/factory default)]})
 
 (defn main
   []
