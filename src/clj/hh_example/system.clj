8beb3292d3598e71a5e84c305d1a42dee914651e
Sameer Rahmani <lxsameer@gnu.org>
2018-02-23 18:55:24 +0000

    The old hh_example ns has been removed

--- src/clj/hh_example/system.clj
+++
@@ -1,8 +0,0 @@
-(ns hh-example.system
-  (:require
-   [hellhound.http       :as http]
-   [hellhound.components.webserver :as webserver]))
-
-
-(def dev-system
-  {:components [(webserver/factory http/default-routes)]})
