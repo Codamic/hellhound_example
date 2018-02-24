5332d2758843b7ac413086c8097b2b0ca1fba253
Sameer Rahmani <lxsameer@gnu.org>
2018-02-24 14:23:47 +0000

    asciidoc points have been added to core ns of proxy example

--- src/clj/getting_started/proxy/core.clj
+++ src/clj/getting_started/proxy/core.clj
@@ -1,20 +1,18 @@
 (ns getting-started.proxy.core
   (:require [getting-started.proxy.system :as system]
-            [hellhound.system :as hh-system]
+            [hellhound.system :as hellhound]
             [aleph.http :as http])
   (:gen-class))
 
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (let [[url _port path] args
         port             (Integer. (or _port "3000"))]
+
     (if (nil? url)
       (println "URL is missing. Please pass the 3rd party URL as the first arg")
-      (let [proxy-system (system/factory port url)]
+      (let [proxy-system (system/factory port url)]       ;; <1>
         (println (str "Starting server on " port "..."))
-        (hh-system/set-system! proxy-system)
-        (hh-system/start!)))))
-
-        ;;(http/start-server (handler url path) {:port port})))))
+        (hellhound/set-system! proxy-system)              ;; <2>
+        (hellhound/start!)))))                            ;; <3>
