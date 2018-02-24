34cb4e93d3e8fd040553e19f349d9ae9f79d94e8
Sameer Rahmani <lxsameer@gnu.org>
2018-02-24 01:35:52 +0000

    proxy/response generator component extracted to its own ns

---
+++ src/clj/getting_started/proxy/components/response.clj
@@ -0,0 +1,22 @@
+(ns getting-started.proxy.components.response
+  (:require [hellhound.component :as hcomp]
+            [manifold.stream :as stream]))
+
+
+(defn make-response
+  [output]
+  (fn [{:keys [index-content] :as event}]
+    (stream/put! output
+                 (assoc event :response {:body index-content
+                                         :headers []
+                                         :status 200}))))
+
+(defn ->response
+  [this context]
+  (let [[input output] (hcomp/io this)]
+    (stream/consume (make-response output) input)
+    this))
+
+(defn ->response-factory
+  []
+  (hcomp/make-component ::->response ->response #(identity %)))
